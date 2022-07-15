2494beea63aa54f3ab55c4bc3bc9165562a771c9
rshkrobot <rodion987@gmail.com>
2022-07-16 02:26:36 +0300

    buildspec.yml init

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
-const {src, dest, watch, series} = require('gulp');
+const {src, dest, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask(){
@@ -19,32 +18,8 @@ function jsTask(){
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
-
-// Browsersync Tasks
-function browsersyncServe(cb){
-    browsersync.init({
-        server: {
-            baseDir: '.'
-        }
-    });
-    cb();
-}
-
-function browsersyncReload(cb){
-    browsersync.reload();
-    cb();
-}
-
-// Watch Task
-function watchTask(){
-    watch('*.html', browsersyncReload);
-    watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
-}
-
 // Default Gulp task
 exports.default = series(
     scssTask,
     jsTask,
-    browsersyncServe,
-    watchTask
 );
