a79977e9e32e528376955b6c12926bbd62ebd5b6
Viktor <viktor@dsi.io>
2022-07-15 17:55:47 +0300

    Version 1

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+function toggleMenu () {
     const menuButton = document.querySelector(".burger-menu");
     const mobileMenu = document.querySelector(".mobile-menu");
     const mobileLogo = document.querySelector(".mobile-header .logo-block");
@@ -6,3 +6,26 @@ function toggleMenu() {
     mobileMenu.classList.toggle("active");
     mobileLogo.classList.toggle("active");
 }
+
+function openBank () {
+    const donatForm = document.querySelector(".donat-form");
+    const remittance = document.querySelector(".remittance");
+    const remittanceButton = document.querySelector(".remittance-button");
+    const bunkButton = document.querySelector(".bank-button");
+    donatForm.classList.add("active");
+    remittance.classList.remove("active");
+    bunkButton.classList.add("active");
+    remittanceButton.classList.remove("active");
+}
+
+function openRemittance () {
+    const donatForm = document.querySelector(".donat-form");
+    const remittance = document.querySelector(".remittance");
+    const remittanceButton = document.querySelector(".remittance-button");
+    const bunkButton = document.querySelector(".bank-button");
+    donatForm.classList.remove("active");
+    remittance.classList.add("active");
+    bunkButton.classList.remove("active");
+    remittanceButton.classList.add("active");
+}
+
