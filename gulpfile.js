f6719cb33f3393f0a5711d9df7b7688a5a8ba6e8
Viktor <viktor@dsi.io>
2022-09-06 20:09:41 +0300

    Fix bug and setup English language as default

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-const {src, dest, series} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
 
 // Sass Task
 function scssTask(){
@@ -18,8 +19,32 @@ function jsTask(){
     .pipe(terser())
     .pipe(dest('dist', { sourcemaps: '.' }));
 }
+
+// Browsersync Tasks
+function browsersyncServe(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    });
+    cb();
+}
+
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask(){
+    watch('*.html', browsersyncReload);
+    watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+}
+
 // Default Gulp task
 exports.default = series(
     scssTask,
     jsTask,
+    browsersyncServe,
+    watchTask
 );
